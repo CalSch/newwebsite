78962e1cf0c6719dedcfd22b8b1da688ae2bed87
CalSch <cal.schwick@gmail.com>
2021-08-09 14:51:34 -0500

    change to php

--- index.php
+++ index.php
@@ -19,13 +19,43 @@
 	</head>
   <body>
 		
+		<noscript>
+			<div class="noscript">
+				<h3>Warning!</h3>
+				<p>This webpage heavily relies on JavaScript which is disabled, please turn on JavaScript.</p>
+			</div>
+		</noscript>
+
 		<!-- Navbar -->
 		<div id="nav-placeholder"></div>
 
 		<div class="container-fluid" style="margin-top:80px">
-			<div class="container-fluid p-3 my-3 bg-primary text-white">
+			<div class="title">
 				<h1>Welcome.</h1>
 				<p>This is my website.</p>
+			</div>
+			<div class="container content-2">
+				<h1>Check out my stuff!</h1><br>
+
+				<div class="row">
+
+					<div class="col-md-4 my-stuff">
+						<h3>My Latest Blog: </h3>
+						<p><a href="/blog/1-nodejs-webserver.html">How to make a webserver in Node.js</a></p>
+					</div>
+					<div class="col-md-4 my-stuff">
+						<h3>My Pictures:</h3>
+						<p>Internet Icon: <img src="/img/WebIcon.svg" width="30px" /><br>
+						Website Logo: <img src="/img/logo.svg" width="30px" /><br>
+						<a href="/pictures/">More</a>
+						
+						</p>
+					</div>
+
+					<div class="col-md-4 my-stuff">
+					</div>
+
+				</div>
 			</div>
 		</div>
 
--- script.js
+++ script.js
@@ -5,3 +5,9 @@ $("#nav-placeholder").hide(() => {
 		},10);
 	});
 });
+
+$("#syntax").html(`
+<link rel="stylesheet" href="/highlight/styles/atom-one-light.css">
+<script src="/highlight/highlight.pack.js"></script>
+<script>hljs.highlightAll();</script>
+`);
